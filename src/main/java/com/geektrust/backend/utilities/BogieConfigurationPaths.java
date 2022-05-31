3edc7f52aef28bc3d0d16399d73f1a33a2d2cc49
CrioUser <crio-user@criodo.com>
2022-05-31 23:00:57 +0530

    added relative path for base folder in BeginConfigurationPaths.java

--- src/main/java/com/geektrust/backend/utilities/BogieConfigurationPaths.java
+++ src/main/java/com/geektrust/backend/utilities/BogieConfigurationPaths.java
@@ -2,7 +2,7 @@ package com.geektrust.backend.utilities;
 
 public enum BogieConfigurationPaths {
 
-    BASEPATH("/home/crio-user/workspace/abhishekinamdar95-ME_GEEKTRUST_BACKEND/src/main/java/com/geektrust/backend/"), 
+    BASEPATH("src/main/java/com/geektrust/backend/"), 
     TRAINs_BOGIE_CONFIG_PATH(BASEPATH.getPath() + "JSONS/trainsBogiesConfigurations.json"),
     TRAIN_A_BOGIE_CONFIG_PATH(BASEPATH.getPath() + "JSONS/train-A-BogiesConfigurations.json"),
     TRAIN_B_BOGIE_CONFIG_PATH(BASEPATH.getPath() + "JSONS/train-B-BogiesConfigurations.json");
